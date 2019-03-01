36a7316ed34e7ae0960c5d749df587b8b3b75664
Pramod Kumar Verma <pramodk51@outlook.com>
2019-03-01 14:54:17 +0530

    Add files via upload

--- PSLab_Original/PSLAB_SD.c
+++ PSLab_Original/PSLAB_SD.c
@@ -5,22 +5,37 @@
 #include "PSLAB_SD.h"
 
 
-void SendCommand(BYTE cmd, unsigned long arg) {
 
-     BYTE send_arr[6]; 
-     send_arr[0] = cmd | 0x40; 
-     send_arr[1] = arg >> 24; 
-     send_arr[2] = arg >> 16; 
-     send_arr[3] = arg >> 8; 
-     send_arr[4] = arg; 
-     send_arr[5] = (SDCRC(send_arr, 5, 0) << 1) | 1; 
-      
-     for(cmd = 0; cmd < sizeof(send_arr); cmd++){ 
-          spi_write8(send_arr[cmd]); 
-     } 
+/**************Helper Functions******************/
 
+inline void SDSelectLow(void){
+    //make the select line low using GPIO
+    //will implement this later after discussing
 }
 
+inline void SDSelectHigh(void){
+   //make the select line high using GPIO
+}
+
+
+
+
+inline BYTE SDReadResp(void){ 
+    
+    //continuously sends the dummy byte 0xFF to the SD card,
+    //until the response back is not busy
+    BYTE v, i = 0;      
+     do{                 
+          v = spi_write8(0xFF); 
+     }while(i++ < 128 && (v == 0xFF));      
+     return v; 
+     
+} 
+
+
+/****************APIs for external world****************/
+//for this CRC function see below link
+//http://nerdclub-uk.blogspot.com/2012/11/how-spi-works-with-sd-card.html
 
 BYTE SDCRC(BYTE* chr,BYTE cnt,BYTE crc){ 
      BYTE i, a; 
@@ -36,5 +51,64 @@ BYTE SDCRC(BYTE* chr,BYTE cnt,BYTE crc){
      }      
      return crc & 0x7F; 
 } 
+
+
+
+BYTE SDSendCommand(BYTE cmd, unsigned long arg) {
+
+    BYTE res;
+    
+    SDSelectHigh();
+    //write a dummy byte can ignored also
+    spi_write8(0xFF);
+    SDSelectLow(); 
+    //spi_write8(0xFF);
+    
+    
+     BYTE send_arr[6]; 
+     send_arr[0] = cmd | 0x40; 
+     send_arr[1] = arg >> 24; 
+     send_arr[2] = arg >> 16; 
+     send_arr[3] = arg >> 8; 
+     send_arr[4] = arg; 
+     send_arr[5] = (SDCRC(send_arr, 5, 0) << 1) | 1; 
+      
+     for(cmd = 0; cmd < sizeof(send_arr); cmd++){ 
+          spi_write8(send_arr[cmd]); 
+     } 
+     
+     //get the response
+     res=SDReadResp();
+     
+     return res;
+}
+/*
+//SD Result structure to be defined
+SDResult SD_Init(SDCardType *x){
+
+
+    // Initialize SPI for use with the memory card
+    initSPI();        //change accordingly
+    SDSelectHigh();
+    //low frequency for initialization
+    //lowClockSPI();     //will implement it later
+        
+        
+    //for initialization 
+    //We must supply at least 74 clocks with CS high
+    BYTE i;
+    for(i = 0; i < 12; i++) { 
+          spi_write8(0xFF);     //lots of clocks to give card time to init 
+     } 
+
+    ////////////////
+    ///////////////
+    
+    
+    
+}
+
+*/
+
 
 
--- PSLab_Original/PSLAB_SD.h
+++ PSLab_Original/PSLAB_SD.h
@@ -14,9 +14,63 @@
 extern "C" {
 #endif
 
-extern void SendCommand(BYTE cmd, unsigned long arg);
-extern BYTE SDCRC(BYTE* chr,BYTE cnt,BYTE crc);
 
+/*Definitions of SD commands */
+#define CMD0    (0x40+0)        /* GO_IDLE_STATE            */
+#define CMD1    (0x40+1)        /* SEND_OP_COND (MMC)       */
+#define ACMD41  (0xC0+41)       /* SEND_OP_COND (SDC)       */
+#define CMD8    (0x40+8)        /* SEND_IF_COND             */
+#define CMD9    (0x40+9)        /* SEND_CSD                 */
+#define CMD10   (0x40+10)       /* SEND CID                 */
+#define CMD16   (0x40+16)       /* SET_BLOCKLEN             */
+#define CMD17   (0x40+17)       /* READ_SINGLE_BLOCK        */
+#define CMD24   (0x40+24)       /* WRITE_SINGLE_BLOCK       */
+#define CMD42   (0x40+42)       /* LOCK_UNLOCK              */
+#define CMD55   (0x40+55)       /* APP_CMD                  */
+#define CMD58   (0x40+58)       /* READ_OCR                 */
+#define CMD59   (0x40+59)       /* CRC_ON_OFF               */
+    
+    
+    
+#define SD_INIT_TRYS    0x03
+
+/* CardType */
+#define SDCT_MMC        0x01                    /* MMC version 3    */
+#define SDCT_SD1        0x02                    /* SD version 1     */
+#define SDCT_SD2        0x04                    /* SD version 2     */
+#define SDCT_SDC        (SDCT_SD1|SDCT_SD2)     /* SD               */
+#define SDCT_BLOCK      0x08                    /* Block addressing */
+
+#define SD_BLK_SIZE     512
+    
+    
+    
+typedef struct {
+     
+    BYTE     Type;            // Card type (detected by SD_Init())
+    BYTE     CSD[16];         // SD card CSD register (card structure data)
+	BYTE     CID[16];         // SD card CID register (card identification number)
+	BYTE     SCR1[8];         // SD card SCR register (SD card configuration)   
+        
+} SDCardType;
+    
+/* Results of SD functions */
+typedef enum {
+    SD_OK =   0,       /* 0: Function succeeded    */
+    SD_NOINIT=1,       /* 1: SD not initialized    */
+    SD_ERROR= 2,       /* 2: Disk error            */
+    SD_PARERR=3,       /* 3: Invalid parameter     */
+    SD_BUSY=  4,       /* 4: Programming busy      */
+    SD_REJECT=5,       /* 5: Reject data           */
+    SD_NORESPONSE=6    /* 6: No response           */
+} SDResult;    
+
+
+
+    
+extern BYTE SDSendCommand(BYTE cmd, unsigned long arg);
+extern BYTE SDCRC(BYTE* chr,BYTE cnt,BYTE crc);
+//extern SDResult SD_Init(SDCardType *x);
 
 
 
